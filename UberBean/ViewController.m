6df7509d03393b3cce2d1e622bd2f43e1838be99
Jun Oh <kyungjunjoh@gmail.com>
2019-01-25 14:57:49 -0800

    App requests for user's current location and starts the map with location zoomed in

--- UberBean/ViewController.m
+++ UberBean/ViewController.m
@@ -7,8 +7,13 @@
 //
 
 #import "ViewController.h"
+#import <MapKit/MapKit.h>
+#import <CoreLocation/CoreLocation.h>
 
-@interface ViewController ()
+@interface ViewController () <CLLocationManagerDelegate, MKMapViewDelegate>
+
+@property (nonatomic) CLLocationManager* locationManager;
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
 
 @end
 
@@ -17,7 +22,33 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.locationManager = [[CLLocationManager alloc] init];
+    
+    [self.locationManager requestWhenInUseAuthorization];
+    
+    self.mapView.showsUserLocation = YES;
+    
+    self.locationManager.delegate = self;
+    self.mapView.delegate = self;
+    
+    MKCoordinateSpan span = MKCoordinateSpanMake(0.06, 0.06);
+    MKCoordinateRegion region = MKCoordinateRegionMake(self.locationManager.location.coordinate, span);
+    [self.mapView setRegion:region animated:YES];
 }
 
+- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray<CLLocation *> *)locations {
+    NSLog(@"updated");
+}
+
+- (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error {
+    NSLog(@"failed");
+}
+
+- (void)locationManager:(CLLocationManager *)manager didChangeAuthorizationStatus:(CLAuthorizationStatus)status {
+    if(status == kCLAuthorizationStatusAuthorizedWhenInUse || status == kCLAuthorizationStatusAuthorizedAlways) {
+        [self.locationManager requestLocation];
+    }
+}
 
 @end
